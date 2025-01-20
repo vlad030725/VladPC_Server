fc46513f9cefcf1a08030691ae52da7af74e7ca1
vlad030725 <udinvladl22@gmail.com>
2025-01-20 12:44:19 +0300

    Добавлены все списки.

--- VladPC.Common/Enums.cs
+++ VladPC.Common/Enums.cs
@@ -28,4 +28,58 @@ namespace VladPC.Common
         AM5,
         sWRX8,
     }
+
+    enum MemoryType
+    {
+        GDDR6X,
+        GDDR6,
+        GDDR5,
+        GDDR3,
+        DDR5,
+        DDR4,
+        DDR3,
+        DDR3L,
+        DDR2,
+    }
+
+    enum Chipset
+    {
+        Z890,
+        Z790,
+        B760,
+        B660,
+        H610,
+        H510,
+        H470,
+        H370,
+        H310,
+        X870,
+        X870E,
+        B850,
+        X670,
+        B650,
+        A620,
+        B550,
+        A520,
+        B450,
+    }
+
+    enum Status80plus
+    {
+        None,
+        Standart,
+        Bronze,
+        Silver,
+        Gold,
+        Platinum,
+        Titanium,
+    }
+
+    enum FormFactor
+    {
+        E_ATX,
+        Standart_ATX,
+        Micro_ATX,
+        Mini_ITX,
+    }
 }
